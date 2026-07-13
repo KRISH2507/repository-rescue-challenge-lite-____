cdc4e805289ce68aeccd7f6d0f8731e3892a55ce
sriman <srimandgl2004@gmail.com>
2026-07-13 12:17:24 +0530

    work

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// Simple starter application.
+// It runs, but it is not finished.
+
+// TODO: replace placeholder implementation
+
+const APP_PORT = 3000; // TODO: read this from environment config instead of hardcoding
+
+function main() {
+  // TODO: replace placeholder implementation
+  const message = "app is running (placeholder)";
+  console.log(message);
+  console.log("listening on port " + APP_PORT);
+}
+
+main();
